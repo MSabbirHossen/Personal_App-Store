1e8de232007fb1e7f844056c192309ca6496dd3e
MSabbirHossen <mshossen724@gmail.com>
2026-01-09 11:48:04 +0300

    Footer Updated as Figma.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,69 @@
-import React from 'react';
+import React from "react";
 import { FaFacebook } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-
+import { Link, Links } from "react-router";
 
 const footerSocialLinks = [
-  {name: 'Facebook', icon: <FaFacebook />, link: 'https://www.facebook.com/profile.php?id=61577697837535'},
-  {name: 'Instagram', icon: <FaInstagramSquare />, link: 'https://www.instagram.com/parttimecoder/'},
-  {name: 'LinkedIn', icon: <FaLinkedin />, link: 'https://www.linkedin.com/in/ms-hossen/'},
-  {name: 'YouTube', icon: <FaYoutube />, link: 'https://www.youtube.com/@Part-TimeCoder'},
+  {
+    name: "Facebook",
+    icon: <FaFacebook />,
+    link: "https://www.facebook.com/profile.php?id=61577697837535",
+  },
+  {
+    name: "Instagram",
+    icon: <FaInstagramSquare />,
+    link: "https://www.instagram.com/parttimecoder/",
+  },
+  {
+    name: "LinkedIn",
+    icon: <FaLinkedin />,
+    link: "https://www.linkedin.com/in/ms-hossen/",
+  },
+  {
+    name: "YouTube",
+    icon: <FaYoutube />,
+    link: "https://www.youtube.com/@Part-TimeCoder",
+  },
 ];
 
 const Footer = () => {
-    return (
-        <div>
-          <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
-  <aside className="grid-flow-col items-center">
-    <img src="/assets/logo.png" alt="App Store Logo" className="w-10 h-10"/>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-  </aside>
-  {
-    <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-      {
-        footerSocialLinks.map((social) => (
-          <a key={social.name} href={social.link} target="_blank" rel="noopener noreferrer" className="text-2xl">{social.icon}</a>
-        ))
-      }
-    </nav>
-  }
-</footer>
+  return (
+    <div>
+      <footer className="p-4 bg-gray-800 text-white mt-10">
+        <div className="flex flex-col md:flex-row md:justify-between w-full px-4">
+          <img
+            src="/assets/logo.png"
+            alt="App Store Logo"
+            className="w-10 h-10"
+          />
+
+          {
+            <nav className="flex gap-4">
+              {footerSocialLinks.map((social) => (
+                <a
+                  key={social.name}
+                  href={social.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-2xl"
+                >
+                  {social.icon}
+                </a>
+              ))}
+            </nav>
+          }
         </div>
-    );
+        <div className="text-center">
+          <p>
+            Copyright © {new Date().getFullYear()} - All right reserved by
+            <Link to="/">Part-time Coder</Link>.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
